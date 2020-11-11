00ed08da3e4571f02eec3722e0f0b3443e4889f8
Duncan Stark <letmunplaysgames@gmail.com>
2020-11-11 21:32:53 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -37,5 +37,5 @@ def aoh_update(aoh, row, key, new_value)
        {:name=>"Pat Monteseian", :occupation=>"Staff"},
        {:name=>"Kate Gompert", :occupation=>"None"},
        {:name=>"Bruce Green", :occupation=>"Fan of Mildred"}]
-       result[2][:name]
+       result[1] = {:name ""
 end
